814fa6e7933c99bedb5c157ef9cbdce318aa9a90
Franco <francocorniglione5@gmail.com>
2023-09-25 19:32:13 -0300

    adding env file

--- characters/index.js
+++ characters/index.js
@@ -1,6 +1,5 @@
 const server = require("./src/server");
-
-const PORT = 8001;
+const { PORT } = require("./src/config/envs");
 
 server.listen(PORT, () => {
   console.log(`Characters service listening on port ${PORT}`);
