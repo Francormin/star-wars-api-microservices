95a2f4cba4bb7d34585efd312963fe7316146b2e
Franco <francocorniglione5@gmail.com>
2023-10-03 16:47:13 -0300

    renaming methods stuff

--- database/src/controllers/getById.js
+++ database/src/controllers/getById.js
@@ -4,6 +4,6 @@ const { response } = require("../utils");
 module.exports = async (req, res) => {
   const { model, id } = req.params;
 
-  const result = await store[model].getById(id);
+  const result = await store[model].getDocById(id);
   return response(res, 200, result);
 };
