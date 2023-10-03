21c1fc9102be2f239cdd2800fe60b6b0bae1bccc
Franco <francocorniglione5@gmail.com>
2023-10-03 16:46:35 -0300

    renaming methods stuff

--- database/src/controllers/getAll.js
+++ database/src/controllers/getAll.js
@@ -4,6 +4,6 @@ const { response } = require("../utils");
 module.exports = async (req, res) => {
   const { model } = req.params;
 
-  const result = await store[model].getAll();
+  const result = await store[model].getAllDocs();
   return response(res, 200, result);
 };
