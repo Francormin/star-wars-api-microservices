d833a339db238dd6d132647397216d363f21e2a9
Franco <francocorniglione5@gmail.com>
2023-10-03 16:30:59 -0300

    updating index with new controllers

--- characters/src/controllers/index.js
+++ characters/src/controllers/index.js
@@ -3,5 +3,7 @@ const { catchedAsync } = require("../utils");
 module.exports = {
   getCharacters: catchedAsync(require("./getCharacters")),
   getCharacterById: catchedAsync(require("./getCharacterById")),
-  createCharacter: catchedAsync(require("./createCharacter"))
+  createCharacter: catchedAsync(require("./createCharacter")),
+  updateCharacter: catchedAsync(require("./updateCharacter")),
+  deleteCharacter: catchedAsync(require("./deleteCharacter"))
 };
