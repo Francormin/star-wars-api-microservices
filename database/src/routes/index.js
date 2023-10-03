e39786165a512749ac43a141898741b4516fad86
Franco <francocorniglione5@gmail.com>
2023-10-03 16:39:46 -0300

    adding new routes

--- database/src/routes/index.js
+++ database/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const { validateModel } = require("../middlewares");
-const { getAll, getById, insert } = require("../controllers");
+const { getAll, getById, insert, update, remove } = require("../controllers");
 
 const router = Router();
 
 router.get("/:model", validateModel, getAll);
 router.get("/:model/:id", validateModel, getById);
 router.post("/:model", validateModel, insert);
+router.patch("/:model/:id", validateModel, update);
+router.delete("/:model/:id", validateModel, remove);
 
 module.exports = router;
