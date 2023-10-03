fdf352c3fba5ae85b6b73a7b62f73caa0fa0633f
Franco <francocorniglione5@gmail.com>
2023-10-03 16:41:37 -0300

    updating index with new controllers

--- database/src/controllers/index.js
+++ database/src/controllers/index.js
@@ -3,5 +3,7 @@ const { catchedAsync } = require("../utils");
 module.exports = {
   getAll: catchedAsync(require("./getAll")),
   getById: catchedAsync(require("./getById")),
-  insert: catchedAsync(require("./insert"))
+  insert: catchedAsync(require("./insert")),
+  update: catchedAsync(require("./update")),
+  remove: catchedAsync(require("./remove"))
 };
